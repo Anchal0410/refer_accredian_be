718378526a2b27b620ffe207589e3f86c345b751
Anchal0410 <jainanchal0410@gmail.com>
2025-02-20 03:33:27 +0530

    deploy update

--- src/app.js
+++ src/app.js
@@ -7,10 +7,18 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['https://refer-accredian-fe.vercel.app/', 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PATCH'],
-    credentials: true
-  }));
+    origin: [
+      'https://refer-accredian-fe.vercel.app',
+      'http://localhost:5173', 
+      
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+})); 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
